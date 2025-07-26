112dc96fd4630e6cbe800dea11c866308fc6ed90
jerious <jerious_withoutvalue@outlook.com>
2025-07-26 16:48:50 -0300

    member access implementation setup

---
+++ beerec/src/codegen/impl/oop/member-access/codegen-mbr-access.c
@@ -0,0 +1,6 @@
+#include "codegen-mbr-access.h"
+
+AsmReturn* generate_member_access(CodeGen* codegen, Node* node, AsmArea* area)
+{
+	return NULL;
+}
--- beerec/src/codegen/impl/oop/member-access/codegen-mbr-access.h
+++ beerec/src/codegen/impl/oop/member-access/codegen-mbr-access.h
@@ -1,4 +1,8 @@
 #ifndef CODEGEN_MBR_ACCESS_H
 #define CODEGEN_MBR_ACCESS_H
 
+#include "../../../codegen.h"
+
+AsmReturn* generate_member_access(CodeGen* codegen, Node* node, AsmArea* area);
+
 #endif
