7d00db9198d65f45001aa18999592eb8c46e3119
jerious <jerious_withoutvalue@outlook.com>
2025-10-09 00:09:44 -0300

    fixed make file

--- beerec/src/init/main.c
+++ beerec/src/init/main.c
@@ -1,8 +1,10 @@
 #include "compiler.h"
+#include "../frontend/modules/modules.h"
 
-// Entry-Point do compiler, argv[1] é o path pro .mod e o argv[1] é o path pro Entry-Point.
+// Entry-Point do compiler, argv[1] é o path pro .mod e o argv[1] é o path pro Entry-Point, e o resto são flags
 int main(int argc, char** argv)
 {
+	ModuleConfig* mod_cfg = handle_module_config(argv[1]);
 	compile_module(NULL, argv, argv[2]);
 	
 	return 0;
